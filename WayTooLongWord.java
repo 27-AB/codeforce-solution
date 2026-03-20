0dd89d57a27317b40df6ae0f5a41f2ca1d518257
Birhane Tamirat <birhanetamirat0@gmail.com>
2026-03-19 22:12:17 -0700

    ADd the way too long word codeforce problem solution.

---
+++ WayTooLongWord.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // number of words
+
+        while (n-- > 0) {
+            String word = sc.next();
+
+            if (word.length() > 10) {
+                // abbreviate
+                System.out.println("" 
+                    + word.charAt(0) 
+                    + (word.length() - 2) 
+                    + word.charAt(word.length() - 1));
+            } else {
+                // print as it is
+                System.out.println(word);
+            }
+        }
+
+        sc.close();
+    }
+}
